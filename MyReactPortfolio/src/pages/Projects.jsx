78f80a140bcb2139ef3d3599920948c62ddf9949
ProgramWithKimta <knguyen4268@gmail.com>
2025-03-30 18:30:37 -0500

    added content to project page plus formatting

--- MyReactPortfolio/src/pages/Projects.jsx
+++ MyReactPortfolio/src/pages/Projects.jsx
@@ -6,7 +6,24 @@ function Projects() {
     return <div className="projects">
         <header className="header-pages">My Projects</header>
         <NavBarPages />
-        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+        <p className="project-panda"><img src="https://img.freepik.com/premium-vector/cute-panda-silhouette_655829-81.jpg?w=740" alt="project-1" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
+        <p className="project-panda"><img src="https://img.freepik.com/premium-vector/panda-silhouette-vector-illustration-art_1186924-649.jpg?w=996" alt="project-1" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
+        <p className="project-panda"><img src="https://img.freepik.com/premium-vector/panda-head-logo_681147-481.jpg?w=740" alt="project-3" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
+        <p className="project-panda"><img src="https://img.freepik.com/premium-vector/panda-graphic-illustration-flat-minimalist-logo_1113360-491.jpg?w=740" alt="project-4" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
+        <p className="project-panda"><img src="https://img.freepik.com/free-vector/vector-black-white-character-panda-logo_474888-2950.jpg?t=st=1743377279~exp=1743380879~hmac=07e39523d0aa0b351d1f4211d3a5278e171b65f236e111f6ae6477953a4befe1&w=740" alt="project-5" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
+        <p className="project-panda"><img src="https://img.freepik.com/premium-vector/cute-panda-eating-banboo-icon_1076610-45418.jpg?w=740" alt="project-6" className="project-photos" />
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+
         <Footer />
     </div>
 }
